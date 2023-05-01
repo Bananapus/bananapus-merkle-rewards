327a81040baa8149fe9ea902d99d4f51d047e56a
viraj124 <virajm72@gmail.com>
2023-05-01 23:15:11 +0530

    chore: add mock token for testing

---
+++ src/mock/MockERC20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+
+    constructor(uint256 amount_) ERC20("", "") {
+        _mint(msg.sender, amount_);
+    }
+}
