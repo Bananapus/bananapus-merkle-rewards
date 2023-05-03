3ee4d21898835f6b6d42a39d147d96ae59b811cf
viraj124 <virajm72@gmail.com>
2023-05-04 00:02:52 +0530

    test: complete coverage

--- test/BananaMerkle.t.sol
+++ test/BananaMerkle.t.sol
@@ -12,15 +12,25 @@ contract BananaMerkle_Unit is Test {
 
     BananaMerkle bananaMerkle;
 
-    bytes32 root = 0xa178a1ba718a2a1ade417f90e7ca571a7fff4707a3639b4932bdddc73659f1ff; // 4761904761904761904 tokens for 
+    bytes32 root = 0xa178a1ba718a2a1ade417f90e7ca571a7fff4707a3639b4932bdddc73659f1ff;
+    bytes32 newRoot = 0x694fd08b0fd824ced0ca9e4617ed454c87d180c0347038663a040660c8b99a3b;
     bytes32[] proof = [
       bytes32(0x5aec4f2d7e5259a3b6f3d63f1d8c4250d66ba45666f356e403b116f56032a392),
       bytes32(0x033a2a3dc993fe3b6b3cc3b6732daab111a77d48f5ad78d492110a4c807c2081),
       bytes32(0x1bd6c175061e5be1e78cf49fcc6d5f1b89225cbfcf568f38f60c52ef141ad42e)
     ];
+    bytes32[] newProof = [
+      bytes32(0x0cecdf269bc0eb9688fcf9a6317e62df8663ded96759b21a44d184de753e9b17),
+      bytes32(0xe21e9cc6e93eb392b7d3f7768fe57f41419d79d66a2174549661ed5636d6bd7d),
+      bytes32(0x9eea3421134f1f311a21b59dca63c759554a5b6bf44112df41e1c5972017a678)
+    ];
+
+    uint256 claimAmount = 4761904761904761904;
+    uint256 newClaimAmount = 4285714285714285714;
+
     address claimer = 0x5427B5141A6CC8228A9E74248F51210380adbaE9;
     MockERC20 token;
-    uint256 claimAmount = 4761904761904761904;
+
 
     // Types need to follow the alphabetical order of the json keys!
     struct ProofToTest {
@@ -81,6 +91,85 @@ contract BananaMerkle_Unit is Test {
         }
 
         vm.prank(address(123));
+        vm.expectRevert(abi.encodeWithSelector(BananaMerkle.BananaMerkle_NothingToClaim.selector));
+        bananaMerkle.claim(claimAmount, _proof);
+    }
+
+
+    function test_ClaimerCannotClaimWithInvalidProof() public {
+        bytes32[] memory _proof = new bytes32[](3);
+        for (uint8 i = 0; i < proof.length; i++) {
+            _proof[i] = proof[i];
+        }
+        _proof[2] = 0x1bd6c175061e5be1e78cf49fcc6d5f1b89225cbfcf568f38f60c52ef141ad44e;
+
+        vm.prank(claimer);
+        vm.expectRevert(abi.encodeWithSelector(BananaMerkle.BananaMerkle_NothingToClaim.selector));
+        bananaMerkle.claim(claimAmount, _proof);
+    }
+
+
+
+    function test_claimerCanClaimOnce_when_root_is_updated() public {
+        bytes32[] memory _proof = new bytes32[](3);
+        for (uint8 i = 0; i < proof.length; i++) {
+            _proof[i] = proof[i];
+        }
+        uint256 _tokenBalanceBeforeClaim = token.balanceOf(claimer);
+
+        vm.prank(claimer);
+        bananaMerkle.claim(claimAmount, _proof);
+
+        uint256 _tokenBalanceAfterClaim = token.balanceOf(claimer);
+        uint256 _diff = _tokenBalanceAfterClaim - _tokenBalanceBeforeClaim;
+        assertEq(_diff, claimAmount);
+
+        vm.roll(block.number + 1);
+        bananaMerkle.updateRoot(newRoot);
+
+        for (uint8 i = 0; i < newProof.length; i++) {
+            _proof[i] = newProof[i];
+        }
+        _tokenBalanceBeforeClaim = token.balanceOf(claimer);
+
+        vm.prank(claimer);
+        bananaMerkle.claim(newClaimAmount, _proof);
+
+        _tokenBalanceAfterClaim = token.balanceOf(claimer);
+        _diff = _tokenBalanceAfterClaim - _tokenBalanceBeforeClaim;
+        assertEq(_diff, newClaimAmount);
+
+        vm.prank(claimer);
+        vm.expectRevert(abi.encodeWithSelector(BananaMerkle.BananaMerkle_AlreadyClaimed.selector));
+        bananaMerkle.claim(newClaimAmount, _proof);
+    }
+
+    function test_nonClaimerCannotClaim_when_root_is_updated() public {
+        vm.roll(block.number + 1);
+        bananaMerkle.updateRoot(newRoot);
+
+        bytes32[] memory _proof = new bytes32[](3);
+        for (uint8 i = 0; i < newProof.length; i++) {
+            _proof[i] = newProof[i];
+        }
+
+        vm.prank(address(123));
+        vm.expectRevert(abi.encodeWithSelector(BananaMerkle.BananaMerkle_NothingToClaim.selector));
+        bananaMerkle.claim(claimAmount, _proof);
+    }
+
+
+    function test_ClaimerCannotClaimWithInvalidProof_when_root_is_updated() public {
+        vm.roll(block.number + 1);
+        bananaMerkle.updateRoot(newRoot);
+
+        bytes32[] memory _proof = new bytes32[](3);
+        for (uint8 i = 0; i < newProof.length; i++) {
+            _proof[i] = newProof[i];
+        }
+        _proof[2] = 0x1bd6c175061e5be1e78cf49fcc6d5f1b89225cbfcf568f38f60c52ef141ad44e;
+
+        vm.prank(claimer);
         vm.expectRevert(abi.encodeWithSelector(BananaMerkle.BananaMerkle_NothingToClaim.selector));
         bananaMerkle.claim(claimAmount, _proof);
     }
